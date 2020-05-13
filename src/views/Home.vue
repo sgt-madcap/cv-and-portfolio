970429e0ec1f1d30f5d1a30775c4c1d54a92bd49
madcap <tkachevn89@gmail.com>
2020-05-14 02:07:35 +0300

    Resume template and content added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -74,69 +74,90 @@
         tenetur totam ullam.
       </p>
     </section>
-    <section id="Resume">
-      <div class="row">
+    <section id="Resume" class="resume">
+      <div class="row row-header">
         <h3>{{ translations.resume }}</h3>
-        <div class="col s6 blue lighten-5">
-          <h5>Hard Skills:</h5>
-          <ul>
-            <li>HTML5</li>
-            <li>CSS3</li>
-            <li>Sass & Less preprocessors</li>
-            <li>CSS libraries: Bootstrap, Materializecss</li>
-            <li>Frameworks: Vue, Vuetify</li>
-            <li>Programming languages: node.js, javascript</li>
-            <li>
-              Database: mongoDB, Firebase Realtime Database, Cloud Firestore
-            </li>
-            <li>...</li>
-          </ul>
+      </div>
+      <div class="row">
+        <div class="col m12 lg6 xl6">
+          <div class="skills">
+            <a class="left top skills-icon"
+              ><img src="/img/signs.svg" alt="hard-skills"
+            /></a>
+
+            <h5>Hard Skills:</h5>
+            <ul>
+              <li>HTML5</li>
+              <li>CSS3</li>
+              <li>Javascript, Typescript, NodeJS</li>
+              <li>VueJS, Nuxt</li>
+              <li>Sass, Less</li>
+              <li>Bootstrap, Materializecss, Vuetify</li>
+              <li>MongoDB, Firebase Realtime Database, Cloud Firestore, SQL</li>
+              <li>Socket.IO</li>
+              <li>REST.API</li>
+              <li>Languages: English, Russian, Serbian</li>
+            </ul>
+          </div>
         </div>
-        <div class="col s6 purple lighten-5">
-          <h5>Soft Skills</h5>
-          <ul>
-            <li>Critical thinking</li>
-            <li>Creativeness</li>
-            <li>Attentiveness</li>
-            <li>Responsibility</li>
-            <li>Perseverance</li>
-            <li>Easy learning</li>
-            <li>Sense of beauty</li>
-            <li>Friendly temper</li>
-            <li class="red-text">
-              Understanding and acting as an intermediary between the work of
-              the back-end developer and the web designer.
-            </li>
-            <li>I know how to google :)</li>
-          </ul>
+        <div class="col m12 lg6 xl6">
+          <div class="skills">
+            <a class="left top skills-icon"
+              ><img src="/img/business.svg" alt="soft-skills"
+            /></a>
+
+            <h5>Soft Skills</h5>
+            <ul>
+              <li>Critical thinking</li>
+              <li>Creativeness</li>
+              <li>Attentiveness</li>
+              <li>Responsibility</li>
+              <li>Perseverance</li>
+              <li>Easy learning</li>
+              <li>Sense of beauty</li>
+              <li>Friendly temper</li>
+              <li>
+                Understanding and acting as an intermediary between the work of
+                the back-end developer and the web designer.
+              </li>
+              <li>I know how to google :)</li>
+            </ul>
+          </div>
         </div>
       </div>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
-        accusantium amet assumenda blanditiis consequuntur dignissimos
-        distinctio dolorem, facere fugiat hic illum laborum maiores molestias
-        nam nulla obcaecati odio placeat praesentium suscipit voluptas? Aliquam
-        aperiam atque deleniti deserunt dignissimos eius esse fugiat incidunt,
-        ipsa iste labore laudantium molestias mollitia necessitatibus obcaecati
-        pariatur quae quaerat quasi qui, quibusdam tempore tenetur, voluptas
-        voluptate. Architecto consectetur dolores iste iure necessitatibus, nemo
-        qui sint? Aspernatur at cupiditate nihil quaerat repellendus sint
-        tenetur! Aliquam aperiam architecto atque autem consequatur consequuntur
-        corporis deleniti dignissimos doloremque ea eligendi eum ex ipsam iure,
-        laboriosam laborum minima nobis nulla quae, quidem reprehenderit
-        repudiandae sequi voluptas! Asperiores blanditiis consectetur
-        consequatur corporis delectus doloremque dolorum eveniet in incidunt
-        labore molestiae nam nisi non nulla officia optio possimus quia
-        reprehenderit sequi sit tenetur unde ut veritatis vitae, voluptatibus! A
-        alias aliquid animi dignissimos doloremque dolorum earum enim esse eum
-        expedita inventore molestiae molestias nesciunt nostrum obcaecati omnis
-        possimus quaerat quibusdam ratione recusandae rem reprehenderit
-        similique sint, suscipit tenetur veniam voluptatibus. Accusamus
-        accusantium animi autem cumque cupiditate debitis doloremque eligendi
-        eos esse, in incidunt ipsam ipsum iste iure laudantium nemo non nulla
-        odio optio possimus quae quisquam quod repellendus reprehenderit tempora
-        tenetur totam ullam.
-      </p>
+      <div class="row">
+        <div class="col s6">
+          <div class="skills">
+            <a class="left top skills-icon"
+              ><img src="/img/certificate.svg" alt="hard-skills"
+            /></a>
+
+            <h5>Certificates:</h5>
+            <ul>
+              <li>Cert1</li>
+              <li>Cert2</li>
+            </ul>
+          </div>
+        </div>
+        <div class="col s6">
+          <div class="skills">
+            <a class="left top skills-icon"
+              ><img src="/img/employment2.svg" alt="soft-skills"
+            /></a>
+
+            <h5>Employment:</h5>
+            <ul>
+              <li>Job 1</li>
+              <li>Job 2</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col download-cv">
+          <button class="btn waves-effect cyan darken-2">Download CV</button>
+        </div>
+      </div>
     </section>
     <section id="Blog">
       <h3>{{ translations.blog }}</h3>
@@ -178,7 +199,80 @@ export default {
 </script>
 
 <style scoped>
-.img-left {
-  display: block;
+ul:not(.browser-default) > li {
+  font-weight: bold;
+  padding: 0.5rem 2rem 0.5rem 1rem;
+  text-align: left;
+  list-style-type: none;
+  border-left: 2px solid black;
+}
+
+ul:not(.browser-default) > li::before {
+  position: relative;
+  width: 15px;
+  height: 15px;
+  display: inline-block;
+  content: '';
+  left: -23px;
+  top: 3px;
+  border-radius: 50%;
+  border: 3px solid black;
+  background-color: white;
+}
+.skills {
+  border: 1px solid gray;
+  width: 100%;
+  display: flow-root;
+  position: relative;
+  padding-left: 2.4rem !important;
+  padding-bottom: 0.5rem;
+  transition: box-shadow 0.5s;
+}
+.skills h5 {
+  text-align: left;
+  margin-bottom: 0 !important;
+  padding-bottom: 2rem;
+  border-left: 2px solid black;
+  padding-left: 2rem;
+}
+.skills ul {
+  margin-top: 0;
+}
+.skills-icon {
+  position: absolute;
+  border-radius: 50%;
+  width: 45px !important;
+  height: 45px !important;
+  left: 1rem !important;
+  top: 1rem !important;
+  background-color: white;
+  box-shadow: none;
+  border: 2px solid black;
+  padding: 5px !important;
+}
+.skills-icon img {
+  width: 100%;
+  height: auto;
+}
+.row-header {
+  margin: 0 !important;
+}
+.row-header h3 {
+  width: 100%;
+}
+.resume .col {
+  padding-top: 10px !important;
+  padding-bottom: 10px !important;
+}
+.resume .row {
+  margin-bottom: 10px;
+  display: flex;
+  flex-wrap: wrap;
+}
+.resume .col {
+  display: flex;
+}
+.download-cv {
+  justify-content: flex-start;
 }
 </style>
